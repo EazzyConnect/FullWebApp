b5aa613b1e31a2162cac21b248ff4bd1fb386078
Isaiah Okon <izaya4real@gmail.com>
2023-09-13 14:53:04 +0100

    render

--- model/db.js
+++ model/db.js
@@ -29,6 +29,6 @@ const Portfolio = new mongoose.model ("portfolio", portfolioSchema);
 mongoose.connect(process.env.MongoDbUrl)
 .then(() => {
  console.log("portfolio database is running!")
-},10000);
+},10000000);
 
 module.exports = {Portfolio, roleEnum}
