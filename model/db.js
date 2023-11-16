f4dfbf5e0b6c3cb7abb0535d4f5239b5addeaabc
Isaiah Okon <izaya4real@gmail.com>
2023-11-16 01:47:53 +0100

    update on db

--- model/db.js
+++ model/db.js
@@ -72,7 +72,7 @@ const Portfolio = new mongoose.model ("portfolio", portfolioSchema);
 
 const connection = async ()=>{
  const MongoDbUrl = process.env.MongoDbUrl
- await mongoose.connect("mongodb://127.0.0.1:27017/portfolio",{
+ await mongoose.connect(MongoDbUrl,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
